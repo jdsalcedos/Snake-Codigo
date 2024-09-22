8f35a5df456b91a348c835e2a3ba8e141f16991b
RyujiDC <ddcuellarc@udistrital.edu.co>
2024-09-22 14:59:53 -0500

    Update codigo.py

--- Codigo/codigo.py
+++ Codigo/codigo.py
@@ -67,3 +67,45 @@ class Snake:
     def reset(self):
         self.body = [Vector2(6, 9), Vector2(5, 9), Vector2(4, 9)]
         self.direction = Vector2(1, 0)
+class Juego:
+	def __init__(self):
+		self.snake = Snake()
+		self.food = Food(self.snake.body)
+		self.state = "RUNNING"
+		self.score = 0
+
+	def draw(self):
+		self.food.draw()
+		self.snake.draw()
+
+	def update(self):
+		if self.state == "RUNNING":
+			self.snake.update()
+			self.check_collision_with_food()
+			self.check_collision_with_edges()
+			self.check_collision_with_tail()
+
+	def check_collision_with_food(self):
+		if self.snake.body[0] == self.food.position:
+			self.food.position = self.food.generate_random_pos(self.snake.body)
+			self.snake.add_segment = True
+			self.score += 1
+			self.snake.eat_sound.play()
+
+	def check_collision_with_edges(self):
+		if self.snake.body[0].x == number_of_cells or self.snake.body[0].x == -1:
+			self.game_over()
+		if self.snake.body[0].y == number_of_cells or self.snake.body[0].y == -1:
+			self.game_over()
+
+	def game_over(self):
+		self.snake.reset()
+		self.food.position = self.food.generate_random_pos(self.snake.body)
+		self.state = "STOPPED"
+		self.score = 0
+		self.snake.wall_hit_sound.play()
+
+	def check_collision_with_tail(self):
+		headless_body = self.snake.body[1:]
+		if self.snake.body[0] in headless_body:
+			self.game_over()
